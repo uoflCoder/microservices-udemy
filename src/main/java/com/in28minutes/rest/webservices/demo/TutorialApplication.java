abc87aecd75fd65af8710d09a253e801d4b23820
uoflcoder <tylorcornett@gmail.com>
2018-12-25 11:35:26 -0500

    Initialize Repository

---
+++ src/main/java/com/in28minutes/rest/webservices/demo/TutorialApplication.java
@@ -0,0 +1,14 @@
+package com.in28minutes.rest.webservices.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TutorialApplication.class, args);
+    }
+}
